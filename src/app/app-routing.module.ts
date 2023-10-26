448d09e284b98ad4fa1afbd4f2bd1415aefe260a
Francisco blanco <francisco.blanco@grupoemi.com>
2023-10-26 08:45:24 -0500

    ADD PROJECT

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,25 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:'',
+    redirectTo: 'customer',
+    pathMatch: 'full'
+  },
+  {
+    path: 'admin',
+    loadChildren: () => import('./Admin/admin.module').then( m => m.AdminModule )
+  },
+  {
+    path:'customer',
+    loadChildren: () => import('./Customer/customer.module').then( m => m.CustomerModule )
+  },
+  {
+    path: '**',
+    redirectTo: 'customer'
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
