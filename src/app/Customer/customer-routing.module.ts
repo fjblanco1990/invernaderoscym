c27cd64f467501e77c0cf1b68300ad4466da3283
fjblanco1990 <fjblanco1990@gmail.com>
2023-10-29 12:07:24 -0500

    add new components

--- src/app/Customer/customer-routing.module.ts
+++ src/app/Customer/customer-routing.module.ts
@@ -1,12 +1,25 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { CustomerComponent } from './customer.component';
+import { HomeComponent } from './home/home.component';
+import { CompanyComponent } from './components/company/company.component';
 
 const routes: Routes = [
   {
     path:'',
-    component: CustomerComponent
+    component: CustomerComponent,
+    children: [
+      {
+        path: 'home',
+        component: HomeComponent
+      },
+      {
+        path: 'company',
+        component: CompanyComponent
+      }
+    ]
   },
+ 
   
 ];
 
--- src/app/Customer/customer.module.ts
+++ src/app/Customer/customer.module.ts
@@ -3,11 +3,23 @@ import { CommonModule } from '@angular/common';
 
 import { CustomerRoutingModule } from './customer-routing.module';
 import { CustomerComponent } from './customer.component';
+import { FooterComponent } from './shared/footer/footer.component';
+import { NavbarComponent } from './shared/navbar/navbar.component';
+import { HomeComponent } from './home/home.component';
+import { SliderComponent } from './components/slider/slider.component';
+import { ExperiencesComponent } from './components/experiences/experiences.component';
+import { CompanyComponent } from './components/company/company.component';
 
 
 @NgModule({
   declarations: [
-    CustomerComponent
+    CustomerComponent,
+    FooterComponent,
+    NavbarComponent,
+    HomeComponent,
+    SliderComponent,
+    ExperiencesComponent,
+    CompanyComponent
   ],
   imports: [
     CommonModule,
